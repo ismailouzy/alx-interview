67add18af9ac2b505ad5cb70a85dc09840a11771
ismailouzy <ismailouzal@gmail.com>
2024-07-25 22:58:11 +0000

    docstring

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,7 +5,10 @@ pascal triagle interview project
 
 
 def pascal_triangle(n):
-
+    """
+    returns a list of lists of integers
+    representing the Pascal’s triangle of n
+    """
     if n <= 0:
         return []
 
