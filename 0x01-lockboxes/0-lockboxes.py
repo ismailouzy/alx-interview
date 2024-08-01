1b12c5ce465aac7c8dd8aa7f02f42e9435960b05
ismailouzy <ismailouzal@gmail.com>
2024-08-01 13:16:46 +0000

    done

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = set([0])
+    keys = set(boxes[0])
+
+    while keys:
+        new_key = keys.pop()
+        if new_key < n and new_key not in unlocked:
+            unlocked.add(new_key)
+            keys.update(boxes[new_key])
+
+    return len(unlocked) == n
